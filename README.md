8ee7b628835ca9a5a1e5ed91ee72397c9c5db00c
Gali Deutsch <deutsch.gali@gmail.com>
2022-12-06 23:36:58 -0500

    updating notebook report

--- README.md
+++ README.md
@@ -1,14 +1,8 @@
-[UPDATE LATER]
+<h1>Ingredient Scanner<h2>
 
-Used tesseract and OpenCV for OCR
+<h2>Setup</h2>
 
-Cosmily API documentation to retrieve EWG Skindeep data and analysis: https://docs.cosmily.com/
-
-Pandas used for organizing data
-
-<h1>Setup</h1>
-
-<b>To run this project install the necessary libraries:</b>
+<b>To run this project, install the necessary libraries:</b>
 
 pip install pytesseract
 
@@ -18,20 +12,27 @@ pip install nltk
 
 pip install pandas
 
-<b>How to change image to analyze</b>
+<b>How to change image for analysis</b>
 
-To change the image to analyze put your image in the ./images path and update image_path to your image's name
+To analyze a different product image, upload your image to the ./images path and update the "image_path" variable to your image's file name. Images should be .png or .jpg/jpeg files.
 
-<h1>About</h1>
+<h2>About</h2>
 
-This project utilizes image processing techniques along with ingredient analysis from comily api to provide an in depth analysis on ingredients
+This project utilizes image processing techniques along with ingredient analysis from the Cosmily API to provide an in-depth analysis of a product's ingredients.
 
-Developed by Gali Deutsch and Nicholas Bogdanovic
+Developed by Gali Deutsch and Nicholas Bogdanovic.
 
-<h1>ToDo</h1>
+<h2>Technologies<h2> 
+
+* pytesseract and OpenCV for OCR
+* Cosmily API to retrieve EWG data and analysis: https://docs.cosmily.com/
+* Pandas
+* NLTK
+
+<h2>To Do</h2>
 
 Given more time the following changes/additions would be made:
 
-1. Add user interface to upload images / use camera to take picture
+1. Add user interface to upload images / use camera to take live images
 
 2. Expand user interface to display ingredient(s) / ingredient characteristics
--- main.py
+++ main.py
@@ -7,26 +7,7 @@ import requests
 import pandas as pd
 
 
-"""
-Steps:
-1. user uploads photo of ingredients ✓        
-2. convert photo to text ✓
-3. convert text to list ✓
-4. call Cosmily API for EWG analysis ✓
-5. return ingredients report to user
-
-Error Handling:
-- user input is a jpg, jpeg, or png file.
-- Blurry Image
-- Ingredients are not found in Cosmily database
-- Image is not of ingredients or does not have any text in it
-
-If extra time:
-- scrape a website for a product's ingredients
-- add dictionary instead of hardcoded phrasing
-"""
-
-class IngredientChecker():
+class IngredientScanner():
     def __init__(self, file_path):
         self.ingredients = None
         self.convert_image_to_text(file_path)
@@ -51,7 +32,7 @@ class IngredientChecker():
         img_rgb = cv2.merge([r,g,b])
 
         # convert image to string
-        text = re.findall(r'[^.,:/\n]+', pytesseract.image_to_string(img_rgb))      # custom_config = r'--oem 3 --psm 6' --> add config param if words are too blurry
+        text = re.findall(r'[^.,:/\n]+', pytesseract.image_to_string(img_rgb))   
         self.preprocess(text)
 
     def analyze(self):
@@ -63,52 +44,77 @@ class IngredientChecker():
         self.analysis = response.json()['analysis']
 
     def report(self):
-        description = f'This product contains {self.analysis["total_ingredients"]} ingredients.'
-        print(description)
+        print(self.analysis['description'])
+        print(f"{self.analysis['text'].split('. ')[1]}.")
 
-        # # TODO: EWG stats
-        # self.analysis["ewg"]
+        print("\nSAFETY OF INGREDIENTS")
+        ingredients_dictlist = []
+        ing_df = pd.DataFrame(columns=['Name','Alias','Decision'])
 
-        # print("DETRIMENTS")
-        # # TODO: loop through and list negatives and harmful
-        # self.analysis["negatives"]
-        # self.analysis["harmful"]
+        ings = []
+        ali = []
+        decis = []
+        for ing in self.analysis['ingredients_table']:
+            ingredients_dictlist.append(ing)
+            ings.append(ing['title'])
+            ali.append(ing['alias'])
+            
+            try:
+                ing['ewg']['decision']
+            except:
+                decis.append('N/A')
+            else:
+                decis.append(ing['ewg']['decision'])
 
-        # print("BENEFITS")
-        # # TODO: loop through and list positives and notables
-        # self.analysis["positives"]
-        # self.analysis["notable"]
 
-        # # TODO: loop though each ingredient in ingredients_table and provide breakdown
-        # print("\nWould you like to view the full ingredients data breakdown? (Y/N)")
-        # show_breakdown = input
-
-        # if show_breakdown.lower() == 'y':
-        #     print(pd.DataFrame(self.analysis["ingredients_table"]))
+        ing_df['Name'] = ings
+        ing_df['Alias'] = ali
+        ing_df['Decision'] = decis
         
-        # # TODO: End report
+        print(ing_df)
+
+
+        print("\nPOSITIVE ASPECTS")
+        pos_df = pd.DataFrame(columns=['Benefit','Ingredient','Description'])
+
+        pos = []
+        desc = []
+        ings = []
+        for positive in self.analysis['positive']:
+            pos.append(self.analysis['positive'][positive]['title'])
+            desc.append(self.analysis['positive'][positive]['description'])
+            ings.append(self.analysis['positive'][positive]['list'][0]['title'])
+
+        pos_df['Benefit'] = pos
+        pos_df['Ingredient'] = ings
+        pos_df['Description'] = desc
+
+        print(pos_df)
+
+        print("\nHARMFUL ASPECTS")
+        harm_df = pd.DataFrame(columns=['Harmful','Ingredient','Description'])
+        name = []
+        desc = []
+        ings = []
+        for h in self.analysis['harmful']:
+            if len(self.analysis['harmful'][h]['list']) > 0:
+                for i in self.analysis['harmful'][h]['list']:
+                    name.append(self.analysis['harmful'][h]['title'])
+                    desc.append(self.analysis['harmful'][h]['description'])
+                    ings.append(i['title'])
+
+        harm_df['Harmful'] = name
+        harm_df['Ingredient'] = ings
+        harm_df['Description'] = desc
+
+        print(harm_df)
             
 
-
-def start():
-    print('Please enter the image file path:')
-    # TODO: test more images and remove default
-    # image_path = input()
-    image_path = './images/sensodyne.png'
-    checker = IngredientChecker(image_path)
-    checker.report()
-    checker = None
-
-
 def main():
-    done = 0
-    print("Ready to check some ingredients?")
-    while done < 1:
-        start()
-        print("Would you like to check another product? (Y/N)")
-        user_input = input()
-        if user_input.lower() != 'y':
-            done += 1
+    image_path = './images/sensodyne.png'
+    scanner = IngredientScanner(image_path)
+    scanner.report()
+    scanner = None
 
 
 if __name__ == '__main__':
